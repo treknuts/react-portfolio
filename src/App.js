d26dae027a23bc2f881677343b97a4c591298c37
Trevor Knutson <trevor.a.knutson@ndsu.edu>
2021-10-18 16:15:02 -0500

    Scroll progress svg broken.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,70 @@
 import "./App.scss";
+import { useState, useEffect, useRef } from "react";
 import Navbar from "./components/navbar";
 import Home from "./components/home";
 import About from "./components/about";
 import Projects from "./components/projects";
-import { Route, Switch } from "react-router";
-import { BrowserRouter } from "react-router-dom";
-import { AnimatePresence } from "framer-motion";
+import { 
+  motion, 
+  useSpring, 
+  useTransform, 
+  useViewportScroll
+} from "framer-motion";
 
 function App() {
+  const app = useRef(null);
+  const [isComplete, setIsComplete] = useState(false);
+  const { scrollYProgress } = useViewportScroll();
+  const yRange = useTransform(scrollYProgress, [0, 0.9], [0, 1]);
+  const pathLength = useSpring(yRange, { stiffness: 400, damping: 90 });
+
+  useEffect(() => {
+    console.log(scrollYProgress.get());
+    yRange.onChange(v => setIsComplete(v >= 1));
+  }, [yRange, scrollYProgress]);
+
+  const homeRef = useRef();
+  const aboutRef = useRef();
+  const projectsRef = useRef();
+
   return (
-    <BrowserRouter>
-      <main className="app">
-        <Navbar />
-        <section className="content">
-          <AnimatePresence>
-            <Switch>
-              <Route exact path="/" component={Home} />
-              <Route exact path="/about" component={About} />
-              <Route exact path="/projects" component={Projects} />
-            </Switch>
-          </AnimatePresence>
+      <main className="app" ref={app}>
+        <Navbar homeRef={homeRef} aboutRef={aboutRef} projectsRef={projectsRef} />
+        <section ref={homeRef} id="home" className="content">
+          <Home/>
         </section>
+        <section ref={aboutRef} id="about" className="content">
+          <About />
+        </section>
+        <section ref={projectsRef} id="projects" className="content">
+          <Projects />
+        </section>
+              <svg className="progress-icon" viewBox="0 0 60 60">
+        <motion.path
+          fill="none"
+          strokeWidth="5"
+          stroke="white"
+          strokeDasharray="0 1"
+          d="M 0, 20 a 20, 20 0 1,0 40,0 a 20, 20 0 1,0 -40,0"
+          style={{
+            pathLength,
+            rotate: 90,
+            translateX: 5,
+            translateY: 5,
+            scaleX: -1 // Reverse direction of line animation
+          }}
+        />
+        <motion.path
+          fill="none"
+          strokeWidth="5"
+          stroke="white"
+          d="M14,26 L 22,33 L 35,16"
+          initial={false}
+          strokeDasharray="0 1"
+          animate={{ pathLength: isComplete ? 1 : 0 }}
+        />
+      </svg>
       </main>
-    </BrowserRouter>
   );
 }
 
--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,5 +1,6 @@
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import { motion } from "framer-motion";
+import { motion, useViewportScroll, useTransform, useSpring } from "framer-motion";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faBars,
@@ -9,26 +10,29 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import "./index.scss";
 
-const Navbar = () => {
+const Navbar = ({ homeRef, aboutRef, projectsRef }) => {
+
+  const executeScroll = (ref) => ref.current.scrollIntoView();
+
   return (
     <nav className="nav">
-      <motion.div whileHover={{ scale: 1.05 }} whileTap={{ scale: 0.95 }}>
-        <Link to="/" className="nav-item">
+       <motion.div whileHover={{ scale: 1.05 }} whileTap={{ scale: 0.95 }}>
+        <div onClick={() => executeScroll(homeRef)} className="nav-item">
           <FontAwesomeIcon icon={faHome} size="lg" />
           <p>home</p>
-        </Link>
+        </div>
       </motion.div>
       <motion.div whileHover={{ scale: 1.05 }} whileTap={{ scale: 0.95 }}>
-        <Link to="/about" className="nav-item">
+        <div onClick={() => executeScroll(aboutRef)} className="nav-item">
           <FontAwesomeIcon icon={faInfoCircle} size="lg" />
           <p>about</p>
-        </Link>
+        </div>
       </motion.div>
       <motion.div whileHover={{ scale: 1.05 }} whileTap={{ scale: 0.95 }}>
-        <Link to="/projects" className="nav-item">
+        <div onClick={() => executeScroll(projectsRef)} className="nav-item">
           <FontAwesomeIcon icon={faClipboardList} size="lg" />
           <p>projects</p>
-        </Link>
+        </div>
       </motion.div>
     </nav>
   );
